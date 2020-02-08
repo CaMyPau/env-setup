4fab76038800b83969a3108fc9d64e6882028f5d
Aleksandr Sabinin <>
2020-02-08 11:26:19 +0100

    Added i3status configuration

--- setup.sh
+++ setup.sh
@@ -16,9 +16,13 @@ i3_setup()
     I3_CONF_DIR="${HOME}/.config/i3/"
     mkdir -p ${I3_CONF_DIR}
     cp ${SCRIPT_DIR}/i3.config  ${I3_CONF_DIR}/config
-    cp ${SCRIPT_DIR}/winmenu.py ${I3_CONF_DIR}
 
+    cp ${SCRIPT_DIR}/winmenu.py ${I3_CONF_DIR}
     sudo pip3 install --upgrade i3-py
+
+    I3STATUS_CONF_DIR="${HOME}/.config/i3status/"
+    mkdir -p ${I3STATUS_CONF_DIR}
+    cp ${SCRIPT_DIR}/i3status.config ${I3STATUS_CONF_DIR}/config
 }
 
 zsh_setup()
